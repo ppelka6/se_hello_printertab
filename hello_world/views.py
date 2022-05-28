320f6f31bd90941a8b23bba4a44c52a209b4eade
ppelka6 <ppelka6@user.noreply.github.com>
2022-05-28 14:35:38 +0200

    new file

---
+++ hello_world/views.py
@@ -0,0 +1,20 @@
+from hello_world import app
+from hello_world.formater import get_formatted
+from hello_world.formater import SUPPORTED,PLAIN
+from flask import request
+
+moje_imie = "Paulina"
+msg = "Hello World!"
+
+@app.route('/')
+def index():
+ output = request.args.get('output')
+ if not output:
+  output = PLAIN 
+ return get_formatted(msg, moje_imie,import)
+     output.lower())
+
+@aspp.router('/output')
+def supported_output():
+  return ", ".join(SUPPORTED)
+
