09aafba4caf95db407838aedb2fb2de1bf7ec551
Paulina <85867849+ppelka6@users.noreply.github.com>
2022-05-29 14:29:33 +0200

    Create views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,20 +1,19 @@
 from hello_world import app
 from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED,PLAIN
+from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Paulina"
+moje_imie = "Beata"
 msg = "Hello World!"
 
 @app.route('/')
 def index():
- output = request.args.get('output')
- if not output:
-  output = PLAIN 
- return get_formatted(msg, moje_imie,import)
-     output.lower())
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, moje_imie,
+                         output.lower())
 
-@aspp.router('/output')
+@app.route('/outputs')
 def supported_output():
-  return ", ".join(SUPPORTED)
-
+    return ", ".join(SUPPORTED)
