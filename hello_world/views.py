c0842ed03d69a075be8fa16f59d5f8544c3874df
Paulina <85867849+ppelka6@users.noreply.github.com>
2022-05-29 14:39:31 +0200

    Update views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Beata"
+moje_imie = "Paulina"
 msg = "Hello World!"
 
 @app.route('/')
